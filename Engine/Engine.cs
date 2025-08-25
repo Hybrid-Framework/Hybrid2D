9e0f396a7f66c466493f320090ed5ac318d2e9a2
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-08-25 17:25:33 +0100

    Squashed commit of the following:
    commit b1fb6a7ce06c30b668b755ce43dada5c8c24fd75
    Author: Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
    Date:   Mon Aug 25 17:20:55 2025 +0100
    
        Development
    
        * Project Setup
        * App
        * Engine
        * Windows
        * Linux
        * MacOS
        * Android
        * IOS
        * Web

---
+++ Engine/Engine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using App;
+
+namespace Engine
+{
+    public static class Engine
+    {
+        public static void Run()
+        {
+            App.App.Run();
+        }
+    }
+}
---
+++ Platforms/MacOS/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Engine.Engine.Run();
+    }
+}
