67339f0f46250a4d08db35cf2bfe575e1bac002c
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-09-06 22:50:57 -0700

    Development
    * MacOS

--- .scripts/MacOS.sh
+++ .scripts/MacOS.sh
@@ -1,5 +1,3 @@
-#MacOS.sh
-
 #!/usr/bin/env bash
 set -euo pipefail
 
@@ -8,149 +6,58 @@ MODULES_DIR="$BASE_DIR/Dependencies/Modules"
 DEPENDENCIES_DIR="$BASE_DIR/Dependencies"
 source "$DEPENDENCIES_DIR/Methods.sh"
 
-
 PLATFORM="MacOS"
 ARCHS=("x86_64" "arm64")
-RIDS=("osx-x64" "osx-arm64")
-MODULES=("SDL2" "SDL2_image" "SDL2_mixer" "SDL2_ttf")
-
+RIDS=("ios-x86_64-maccatalyst" "ios-arm64-maccatalyst")
+MODULES=("SDL2")
+MACCATALYST_DEPLOYMENT_TARGET=14.2
 
 SDL2()
 {
   local INDEX="$1"
   local ARCH="${ARCHS[$INDEX]}"
   local RID="${RIDS[$INDEX]}"
-  
+
   Install "SDL2" "https://github.com/libsdl-org/SDL.git" "release-2.32.10"
-  
+
   cd "$MODULES_DIR/$MODULE" || exit
   BUILDPATH="$MODULES_DIR/$MODULE/build_$PLATFORM-$ARCH"
   rm -rf "$BUILDPATH"
   mkdir -p "$BUILDPATH"
-  
+
   xcodebuild \
     -project "$MODULES_DIR/SDL2/Xcode/SDL/SDL.xcodeproj" \
-    -scheme "Framework" \
+    -scheme "Framework-iOS" \
     -configuration Release \
     -arch "$ARCH" \
-    -sdk macosx \
-    MACOSX_DEPLOYMENT_TARGET=10.13 \
+    SUPPORTS_MACCATALYST=YES \
+    MACOSX_DEPLOYMENT_TARGET=$MACCATALYST_DEPLOYMENT_TARGET \
     CONFIGURATION_BUILD_DIR="$BUILDPATH" \
     OTHER_CFLAGS="-Wno-shorten-64-to-32 -Wdeprecated-declarations -DGLES_SILENCE_DEPRECATION" \
     build
-    
-  Transfer "$BUILDPATH/$MODULE.framework/$MODULE" "$BASE_DIR/../Natives/$PLATFORM/$RID"
-  Rename "$BASE_DIR/../Natives/$PLATFORM/$RID/$MODULE" "$BASE_DIR/../Natives/$PLATFORM/$RID/lib$MODULE.dylib"
-}
-
-
-SDL2_image()
-{
-  local INDEX="$1"
-  local ARCH="${ARCHS[$INDEX]}"
-  local RID="${RIDS[$INDEX]}"
-  
-  Install "SDL2_image" "https://github.com/libsdl-org/SDL_image.git" "release-2.8.8"
-  
-  cd "$MODULES_DIR/$MODULE" || exit
-  BUILDPATH="$MODULES_DIR/$MODULE/build_$PLATFORM-$ARCH"
-  rm -rf "$BUILDPATH"
-  mkdir -p "$BUILDPATH"
-  
-  SDL_BUILD="$MODULES_DIR/SDL2/build_$PLATFORM-$ARCH"
-  SDL_FRAMEWORK="$SDL_BUILD/SDL2.framework"
-  SDL_INCLUDE="$SDL_FRAMEWORK/Headers"
-
-  xcodebuild \
-    -project "$MODULES_DIR/SDL2_image/Xcode/SDL_image.xcodeproj" \
-    -scheme "Framework" \
-    -configuration Release \
-    -arch "$ARCH" \
-    -sdk macosx \
-    MACOSX_DEPLOYMENT_TARGET=10.13 \
-    CONFIGURATION_BUILD_DIR="$BUILDPATH" \
-    HEADER_SEARCH_PATHS="$SDL_INCLUDE" \
-    FRAMEWORK_SEARCH_PATHS="$SDL_BUILD" \
-    OTHER_LDFLAGS="-framework SDL2" \
-    OTHER_CFLAGS="-Wno-shorten-64-to-32 -Wdeprecated-declarations -DGLES_SILENCE_DEPRECATION" \
-    build
-    
-  Transfer "$BUILDPATH/$MODULE.framework/$MODULE" "$BASE_DIR/../Natives/$PLATFORM/$RID"
-  Rename "$BASE_DIR/../Natives/$PLATFORM/$RID/$MODULE" "$BASE_DIR/../Natives/$PLATFORM/$RID/lib$MODULE.dylib"
-}
-
-SDL2_mixer()
-{
-  local INDEX="$1"
-  local ARCH="${ARCHS[$INDEX]}"
-  local RID="${RIDS[$INDEX]}"
-  
-  Install "SDL2_mixer" "https://github.com/libsdl-org/SDL_mixer.git" "release-2.8.1"
-  
-  cd "$MODULES_DIR/$MODULE" || exit
-  BUILDPATH="$MODULES_DIR/$MODULE/build_$PLATFORM-$ARCH"
-  rm -rf "$BUILDPATH"
-  mkdir -p "$BUILDPATH"
-  
-  SDL_BUILD="$MODULES_DIR/SDL2/build_$PLATFORM-$ARCH"
-  SDL_FRAMEWORK="$SDL_BUILD/SDL2.framework"
-  SDL_INCLUDE="$SDL_FRAMEWORK/Headers"
-
-  xcodebuild \
-    -project "$MODULES_DIR/SDL2_mixer/Xcode/SDL_mixer.xcodeproj" \
-    -scheme "Framework" \
-    -configuration Release \
-    -arch "$ARCH" \
-    -sdk macosx \
-    MACOSX_DEPLOYMENT_TARGET=10.13 \
-    CONFIGURATION_BUILD_DIR="$BUILDPATH" \
-    HEADER_SEARCH_PATHS="$SDL_INCLUDE" \
-    FRAMEWORK_SEARCH_PATHS="$SDL_BUILD" \
-    OTHER_LDFLAGS="-framework SDL2" \
-    OTHER_CFLAGS="-Wno-shorten-64-to-32 -Wdeprecated-declarations -DGLES_SILENCE_DEPRECATION" \
-    build
-    
-  Transfer "$BUILDPATH/$MODULE.framework/$MODULE" "$BASE_DIR/../Natives/$PLATFORM/$RID"
-  Rename "$BASE_DIR/../Natives/$PLATFORM/$RID/$MODULE" "$BASE_DIR/../Natives/$PLATFORM/$RID/lib$MODULE.dylib"
-}
-
-SDL2_ttf()
-{
-  local INDEX="$1"
-  local ARCH="${ARCHS[$INDEX]}"
-  local RID="${RIDS[$INDEX]}"
-  
-  Install "SDL2_ttf" "https://github.com/libsdl-org/SDL_ttf.git" "release-2.24.0"
-  
-  cd "$MODULES_DIR/$MODULE" || exit
-  BUILDPATH="$MODULES_DIR/$MODULE/build_$PLATFORM-$ARCH"
-  rm -rf "$BUILDPATH"
-  mkdir -p "$BUILDPATH"
-  
-  SDL_BUILD="$MODULES_DIR/SDL2/build_$PLATFORM-$ARCH"
-  SDL_FRAMEWORK="$SDL_BUILD/SDL2.framework"
-  SDL_INCLUDE="$SDL_FRAMEWORK/Headers"
-
-  xcodebuild \
-    -project "$MODULES_DIR/SDL2_ttf/Xcode/SDL_ttf.xcodeproj" \
-    -scheme "Framework" \
-    -configuration Release \
-    -arch "$ARCH" \
-    -sdk macosx \
-    MACOSX_DEPLOYMENT_TARGET=10.13 \
-    CONFIGURATION_BUILD_DIR="$BUILDPATH" \
-    HEADER_SEARCH_PATHS="$SDL_INCLUDE" \
-    FRAMEWORK_SEARCH_PATHS="$SDL_BUILD" \
-    OTHER_LDFLAGS="-framework SDL2" \
-    OTHER_CFLAGS="-Wno-shorten-64-to-32 -Wdeprecated-declarations -DGLES_SILENCE_DEPRECATION" \
-    build
-    
-  Transfer "$BUILDPATH/$MODULE.framework/$MODULE" "$BASE_DIR/../Natives/$PLATFORM/$RID"
-  Rename "$BASE_DIR/../Natives/$PLATFORM/$RID/$MODULE" "$BASE_DIR/../Natives/$PLATFORM/$RID/lib$MODULE.dylib"
 }
 
 COMPLETE()
 {
+  for MODULE in "${MODULES[@]}"; do
+
+    XCFRAMEWORK="$BASE_DIR/../Natives/MacOS/$MODULE.xcframework"
+    rm -rf "$XCFRAMEWORK"
+    mkdir -p "$XCFRAMEWORK"
+    
+    mkdir -p "$XCFRAMEWORK/ios-x86_64-maccatalyst/$MODULE.framework"
+    mkdir -p "$XCFRAMEWORK/ios-arm64-maccatalyst/$MODULE.framework"
+    
+    find "$MODULES_DIR/$MODULE/build_MacOS-x86_64/$MODULE.framework" \
+         -type f \( -name "$MODULE" -o -name "Info.plist" \) \
+         -exec cp {} "$XCFRAMEWORK/ios-x86_64-maccatalyst/$MODULE.framework/" \;
+    
+    find "$MODULES_DIR/$MODULE/build_MacOS-arm64/$MODULE.framework" \
+         -type f \( -name "$MODULE" -o -name "Info.plist" \) \
+         -exec cp {} "$XCFRAMEWORK/ios-arm64-maccatalyst/$MODULE.framework/" \;
+
+  done
+
   read -p "Build complete."
 }
 
