83e54ee575d3a2be36bca1d63780c2c628ef743b
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-09-08 14:17:03 +0100

    Development
    * Windows
    * Android

--- .scripts/Android.sh
+++ .scripts/Android.sh
@@ -10,7 +10,7 @@ ANDROID_NDK="$HOME/AppData/Local/Android/Sdk/ndk/21.4.7075529/build/cmake/androi
 PLATFORM="Android"
 ARCHS=("armeabi-v7a" "arm64-v8a" "x86" "x86_64")
 RIDS=("armeabi-v7a" "arm64-v8a" "x86" "x86_64")
-MODULES=("SDL2")
+MODULES=("SDL2" "SDL2_mixer")
 
 SDL2()
 {
@@ -41,7 +41,49 @@ SDL2()
   cmake --build . --config Release
   cmake --install . --config Release
   
-  Transfer "$MODULES_DIR/$MODULE/install_${PLATFORM}-$ARCH/lib/libSDL2.so" "$BASE_DIR/../Natives/$PLATFORM/$RID/libSDL2.so"
+  Transfer "$MODULES_DIR/$MODULE/install_${PLATFORM}-$ARCH/lib/lib$MODULE.so" "$BASE_DIR/../Natives/$PLATFORM/$RID"
+}
+
+SDL2_mixer()
+{
+  local INDEX="$1"
+  local ARCH="${ARCHS[$INDEX]}"
+  local RID="${RIDS[$INDEX]}"
+  
+  Install "SDL2_mixer" "https://github.com/libsdl-org/SDL_mixer.git" "release-2.8.1"
+  
+  cd "$MODULES_DIR/$MODULE" || exit
+  BUILDPATH="$MODULES_DIR/$MODULE/build_$PLATFORM-$ARCH"
+  INSTALLPATH="$MODULES_DIR/$MODULE/install_$PLATFORM-$ARCH"
+  rm -rf "$BUILDPATH" "$INSTALLPATH"
+  mkdir -p "$BUILDPATH" "$INSTALLPATH"
+  cd "$BUILDPATH" || exit
+  
+  cmake .. -G Ninja -Wno-dev \
+    -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK" \
+    -DANDROID_ABI=$ARCH \
+    -DANDROID_PLATFORM=android-21 \
+    -DSDL2MIXER_WAVE=ON \
+    -DSDL2MIXER_MP3=ON \
+    -DSDL2MIXER_OGG=ON \
+    -DSDL2MIXER_OPUS=OFF \
+    -DSDL2MIXER_FLAC=OFF \
+    -DSDL2MIXER_MOD=OFF \
+    -DSDL2MIXER_MIDI=OFF \
+    -DSDL2MIXER_WAVPACK=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DSDL2MIXER_SAMPLES=OFF \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DCMAKE_INSTALL_PREFIX=$INSTALLPATH \
+    -DCMAKE_C_FLAGS="-Wno-deprecated-declarations" \
+    -DCMAKE_CXX_FLAGS="-Wno-deprecated-declarations" \
+    -DSDL2_INCLUDE_DIR=$MODULES_DIR/SDL2/install_$PLATFORM-$ARCH/include/SDL2 \
+    -DSDL2_LIBRARY=$MODULES_DIR/SDL2/install_$PLATFORM-$ARCH/lib/libSDL2.so
+
+  cmake --build . --config Release
+  cmake --install . --config Release
+  
+  Transfer "$MODULES_DIR/$MODULE/install_${PLATFORM}-$ARCH/lib/lib$MODULE.so" "$BASE_DIR/../Natives/$PLATFORM/$RID"
 }
 
 COMPLETE()
--- .scripts/Windows.sh
+++ .scripts/Windows.sh
@@ -9,7 +9,7 @@ source "$DEPENDENCIES_DIR/Methods.sh"
 PLATFORM="Windows"
 ARCHS=("x64" "win32" "arm64")
 RIDS=("win-x64" "win-x86" "win-arm64")
-MODULES=("SDL2")
+MODULES=("SDL2" "SDL2_mixer")
 
 SDL2()
 {
@@ -41,7 +41,44 @@ SDL2()
   cmake --build . --config Release
   cmake --install . --config Release
   
-  Transfer "$MODULES_DIR/$MODULE/install_${PLATFORM}-$ARCH/bin/SDL2.dll" "$BASE_DIR/../Natives/$PLATFORM/$RID/SDL2.dll"
+  Transfer "$MODULES_DIR/$MODULE/install_${PLATFORM}-$ARCH/bin/$MODULE.dll" "$BASE_DIR/../Natives/$PLATFORM/$RID"
+}
+
+SDL2_mixer()
+{
+  local INDEX="$1"
+  local ARCH="${ARCHS[$INDEX]}"
+  local RID="${RIDS[$INDEX]}"
+  
+  Install "SDL2_mixer" "https://github.com/libsdl-org/SDL_mixer.git" "release-2.8.1"
+  
+  cd "$MODULES_DIR/$MODULE" || exit
+  BUILDPATH="$MODULES_DIR/$MODULE/build_$PLATFORM-$ARCH"
+  INSTALLPATH="$MODULES_DIR/$MODULE/install_$PLATFORM-$ARCH"
+  rm -rf "$BUILDPATH" "$INSTALLPATH"
+  mkdir -p "$BUILDPATH" "$INSTALLPATH"
+  cd "$BUILDPATH" || exit
+  
+  cmake .. -G "Visual Studio 17 2022" -A $ARCH -Wno-dev \
+    -DSDL2MIXER_WAVE=ON \
+    -DSDL2MIXER_MP3=ON \
+    -DSDL2MIXER_OGG=ON \
+    -DSDL2MIXER_OPUS=OFF \
+    -DSDL2MIXER_FLAC=OFF \
+    -DSDL2MIXER_MOD=OFF \
+    -DSDL2MIXER_MIDI=OFF \
+    -DSDL2MIXER_WAVPACK=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DSDL2MIXER_SAMPLES=OFF \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DCMAKE_INSTALL_PREFIX=$INSTALLPATH \
+    -DSDL2_INCLUDE_DIR=$MODULES_DIR/SDL2/install_$PLATFORM-$ARCH/include/SDL2 \
+    -DSDL2_LIBRARY=$MODULES_DIR/SDL2/install_$PLATFORM-$ARCH/lib/SDL2.lib
+
+  cmake --build . --config Release
+  cmake --install . --config Release
+  
+  Transfer "$MODULES_DIR/$MODULE/install_${PLATFORM}-$ARCH/bin/$MODULE.dll" "$BASE_DIR/../Natives/$PLATFORM/$RID"
 }
 
 COMPLETE()
