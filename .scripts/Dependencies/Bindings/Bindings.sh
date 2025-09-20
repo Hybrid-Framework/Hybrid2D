4c2ba0e439278866f53ea536d1580e0efb236731
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-09-20 21:51:28 +0100

    Bindings
    * Generate C# bindings
    * Definitely needs work to be type friendly
    * Possibly use a script to generate helper methods

--- .scripts/Dependencies/Bindings/Bindings.sh
+++ .scripts/Dependencies/Bindings/Bindings.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
 set +e
 
+# dotnet new tool-manifest
+# dotnet tool install -g ClangSharpPInvokeGenerator --version 20.1.2.1
+
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-MODULES_DIR="$BASE_DIR/../Dependencies/Modules"
-DEPENDENCIES_DIR="$BASE_DIR/../Dependencies"
-PROJECT_DIR="$BASE_DIR/Bindings"
-OUTPUT_DIR="$BASE_DIR/Generated"
+MODULES_DIR="$BASE_DIR/../Modules"
+DEPENDENCIES_DIR="$BASE_DIR/.."
 source "$DEPENDENCIES_DIR/Methods.sh"
 
 PLATFORM="Bindings"
-MODULES=("CLANG" "SDL2" "SDL2_image" "SDL2_mixer" "SDL2_ttf")
-SDLINCLUDE="$MODULES_DIR/SDL2/Include"
+MODULES=("SDL2" "SDL2_image" "SDL2_mixer" "SDL2_ttf")
+OUTPUT="$BASE_DIR/Generated"
 EXTENSION=".cs"
 
-rm -rf "$OUTPUT_DIR"
-mkdir -p "$OUTPUT_DIR"
+rm -rf "$OUTPUT"
+mkdir -p "$OUTPUT"
 
 BASE_COMMANDS=(
-  -n SDL2
-  --config latest-codegen
+  ClangSharpPInvokeGenerator
   
-  --remap "SDL_Window*=IntPtr"
-  --remap "SDL_Texture*=IntPtr"
-  --remap "SDL_Renderer*=IntPtr"
+  --namespace SDL2
+  --config latest-codegen
   
   --remap "void*=IntPtr"
   --remap "char=byte"
@@ -37,27 +36,11 @@ BASE_COMMANDS=(
   --nativeTypeNamesToStrip "unsigned int"
 )
 
-CLANG()
-{
-  Install "Clang" "https://github.com/dotnet/ClangSharp.git" "v20.1.2.1"
-  
-  if [[ ! -d "$MODULES_DIR/Clang/artifacts/bin/sources/ClangSharpPInvokeGenerator/Release" ]]; then
-      cd "$MODULES_DIR/Clang/sources/ClangSharpPInvokeGenerator"
-      dotnet build -c Release
-  fi
-  
-  CLANGSHARP=$(find "$MODULES_DIR/Clang/artifacts/bin/sources/ClangSharpPInvokeGenerator/Release" -type f -name "ClangSharpPInvokeGenerator.exe" | head -n 1)
-
-  if [[ -z "$CLANGSHARP" ]]; then
-      echo "ERROR: ClangSharpPInvokeGenerator not found"
-      exit 1
-  fi
-}
-
 SDL2()
 {
   Install "$MODULE" "https://github.com/libsdl-org/SDL.git" "release-2.32.10"
   local INCLUDE="$MODULES_DIR/$MODULE/Include"
+  SDLINCLUDE=$INCLUDE
   
   local FILES=(
     SDL.h
@@ -90,13 +73,12 @@ SDL2()
   for file in "${FILES[@]}"; do
     
       local EXEC=(
-          "$CLANGSHARP"
           "${BASE_COMMANDS[@]}"
           -l $MODULE
           -m $MODULE
           -I "$SDLINCLUDE"
           -f "$INCLUDE/$file"
-          -o "$OUTPUT_DIR/${file%.h}$EXTENSION"
+          -o "$OUTPUT/$MODULE/${file%.h}$EXTENSION"
       )
   
       echo "Generating bindings for $file..."
@@ -123,13 +105,12 @@ SDL2_image()
   for file in "${FILES[@]}"; do
     
       local EXEC=(
-          "$CLANGSHARP"
           "${BASE_COMMANDS[@]}"
           -l $MODULE
           -m $MODULE
           -I "$SDLINCLUDE"
           -f "$INCLUDE/$file"
-          -o "$OUTPUT_DIR/${file%.h}$EXTENSION"
+          -o "$OUTPUT/$MODULE/${file%.h}$EXTENSION"
       )
   
       echo "Generating bindings for $file..."
@@ -156,13 +137,12 @@ SDL2_mixer()
   for file in "${FILES[@]}"; do
     
       local EXEC=(
-          "$CLANGSHARP"
           "${BASE_COMMANDS[@]}"
           -l $MODULE
           -m $MODULE
           -I "$SDLINCLUDE"
           -f "$INCLUDE/$file"
-          -o "$OUTPUT_DIR/${file%.h}$EXTENSION"
+          -o "$OUTPUT/$MODULE/${file%.h}$EXTENSION"
       )
   
       echo "Generating bindings for $file..."
@@ -189,13 +169,12 @@ SDL2_ttf()
   for file in "${FILES[@]}"; do
     
       local EXEC=(
-          "$CLANGSHARP"
           "${BASE_COMMANDS[@]}"
           -l $MODULE
           -m $MODULE
           -I "$SDLINCLUDE"
           -f "$INCLUDE/$file"
-          -o "$OUTPUT_DIR/${file%.h}$EXTENSION"
+          -o "$OUTPUT/$MODULE/${file%.h}$EXTENSION"
       )
   
       echo "Generating bindings for $file..."
@@ -212,12 +191,6 @@ SDL2_ttf()
 
 COMPLETE()
 {
-#  echo
-#  echo "Running C to C# Parsing"
-#  echo
-#  
-#  dotnet run --project "$PROJECT_DIR/Bindings.csproj"
-  
   echo
   read -p "Bindings Generated."
   echo
