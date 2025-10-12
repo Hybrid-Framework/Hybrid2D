869d2c8250c598daf1ae89fa804759907ad383cd
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-10-12 10:07:39 +0100

    Bindings

--- App/Game.cs
+++ App/Game.cs
@@ -2,17 +2,10 @@
 
 namespace App
 {
-    public unsafe class Game : Behaviour
+    public class Game : Behaviour
     {
         public override void Init()
         {
-            if (!SDL.Init(SDL.InitFlags.Everything))
-            {
-                throw new Exception(SDL.GetError());
-            }
-
-            var window = SDL.CreateWindow("title", 600, 400, SDL.WindowFlags.HighPixelDensity);
-            
             
         }
         
--- Engine/Framework/Internal/SDL/SDL.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-public static unsafe partial class SDL
-{
-    public const string library = "SDL3";
-}
