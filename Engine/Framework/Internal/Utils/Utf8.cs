86652ec38cdbd3cdb3d6dcbf5a8675e9fb3d72c3
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-10-13 09:08:37 +0100

    Bindings

---
+++ Engine/Framework/Internal/Utils/Utf8.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+public static unsafe partial class SDL
+{
+    public static string Utf8ToString(byte* ptr, bool free = false)
+    {
+        string text = Marshal.PtrToStringUTF8((IntPtr)ptr);
+        if (free) SDL.Free((IntPtr)ptr);
+        return text;
+    }
+    
+    public static byte[] StringToUtf8(string data)
+    {
+        return System.Text.Encoding.UTF8.GetBytes(data + '\0');
+    }
+}
