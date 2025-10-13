0a36bf1fa81829089854ac052418e91466a6d4a2
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-10-13 09:49:54 +0100

    Bindings

--- App/Game.cs
+++ App/Game.cs
@@ -4,9 +4,16 @@ namespace App
 {
     public unsafe class Game : Behaviour
     {
+        private SDL.Window* window;
+        
         public override void Init()
         {
-            
+            if (!SDL.Init(SDL.InitFlags.Everything))
+            {
+                throw new Exception();
+            }
+
+            window = SDL.CreateWindow("Hello World", 600, 400, SDL.WindowFlags.HighPixelDensity);
         }
         
         public override void Update()
---
+++ Engine/Framework/Internal/SDL/Core/Rect.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+public static unsafe partial class SDL
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Rect
+    {
+        public int x;
+        public int y;
+        public int w;
+        public int h;
+    }
+}
