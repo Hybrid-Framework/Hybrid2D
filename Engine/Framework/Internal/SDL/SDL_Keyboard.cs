2ad9524a6e9e5279214445bd7c6c6f54caeac0db
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-10-15 10:43:52 +0100

    Bindings

---
+++ Engine/Framework/Internal/SDL/SDL_Keyboard.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+public static unsafe partial class SDL
+{
+    // Has Keyboard
+    [DllImport(library, CallingConvention = CallingConvention.Cdecl)]
+    private static extern SDL.Bool SDL_HasKeyboard();
+    public static bool HasKeyboard()
+    {
+        return SDL_HasKeyboard();
+    }
+    
+    // Get Key Name
+    [DllImport(library, CallingConvention = CallingConvention.Cdecl)]
+    private static extern byte* SDL_GetKeyName(SDL.KeyCode keyCode);
+    public static string GetKeyName(SDL.KeyCode keyCode)
+    {
+        return Utf8ToString(SDL_GetKeyName(keyCode));
+    }
+    
+    // Get Key From Name
+    [DllImport(library, CallingConvention = CallingConvention.Cdecl)]
+    private static extern SDL.KeyCode SDL_GetKeyFromName(byte* name);
+    public static SDL.KeyCode GetKeyFromName(string name)
+    {
+        var bytes = StringToUtf8(name);
+
+        fixed (byte* utf8 = bytes)
+        {
+            return SDL_GetKeyFromName(utf8);
+        }
+    }
+}
---
+++ Engine/Framework/Internal/SDL/SDL_Mouse.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+public static unsafe partial class SDL
+{
+    // Has Mouse
+    [DllImport(library, CallingConvention = CallingConvention.Cdecl)]
+    private static extern SDL.Bool SDL_HasMouse();
+    public static bool HasMouse()
+    {
+        return SDL_HasMouse();
+    }
+    
+    // Show Cursor
+    [DllImport(library, CallingConvention = CallingConvention.Cdecl)]
+    private static extern SDL.Bool SDL_ShowCursor();
+    public static bool ShowCursor()
+    {
+        return SDL_ShowCursor();
+    }
+    
+    // Hide Cursor
+    [DllImport(library, CallingConvention = CallingConvention.Cdecl)]
+    private static extern SDL.Bool SDL_HideCursor();
+    public static bool HideCursor()
+    {
+        return SDL_HideCursor();
+    }
+}
