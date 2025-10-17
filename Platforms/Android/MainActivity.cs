b15baf5780e415783cead1caefb00a340a31c032
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2025-10-17 23:03:28 +0100

    Project

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,10 +1,11 @@
 using Android.Content.PM;
 using Hybrid.Platforms;
+using Org.Libsdl.App;
 using Hybrid;
 using App;
 
 [Activity(
-    Label = "Hybrid", 
+    Label = "Hybrid",
     Exported = true,
     MainLauncher = true,
     HardwareAccelerated = true,
@@ -13,7 +14,13 @@ using App;
     Theme = "@android:style/Theme.NoTitleBar.Fullscreen",
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize
 )]
-public class MainActivity : Activity
+public class MainActivity : SDLActivity
 {
+    protected override string[] GetLibraries() => ["SDL3", "SDL3_image", "SDL3_mixer", "SDL3_ttf"];
     
+    protected override void Main()
+    {
+        Platform.Create(new PlatformAndroid(new Game()));
+        Platform.Current.Run();
+    }
 }
--- Platforms/IOS/Main.cs
+++ Platforms/IOS/Main.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Hybrid.Platforms;
 using Hybrid;
 using App;
@@ -6,6 +7,20 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        var assembly = typeof(SDL).Assembly;
+
+        NativeLibrary.SetDllImportResolver(assembly, (library, asm, path) =>
+        {
+            return library switch
+            {
+                "SDL3_image" => NativeLibrary.Load("@rpath/SDL3_image.framework/SDL3_image", asm, path),
+                "SDL3_mixer" => NativeLibrary.Load("@rpath/SDL3_mixer.framework/SDL3_mixer", asm, path),
+                "SDL3_ttf" => NativeLibrary.Load("@rpath/SDL3_ttf.framework/SDL3_ttf", asm, path),
+                "SDL3" => NativeLibrary.Load("@rpath/SDL3.framework/SDL3", asm, path),
+                _ => IntPtr.Zero
+            };
+        });
+
         SDL.MainFunction entry = Entry;
         SDL.RunApp(0, IntPtr.Zero, entry, IntPtr.Zero);
     }
