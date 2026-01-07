e84481e352c95e8f9d48177f9ca569792a4f40e2
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-01-07 04:18:16 +0000

    SDL Callbacks

--- Engine/Framework/Internal/SDL3/SDL/SDL_Main.cs
+++ Engine/Framework/Internal/SDL3/SDL/SDL_Main.cs
@@ -3,9 +3,44 @@ using System;
 
 internal static unsafe partial class SDL
 {
+    // Store (Prevent GC)
+    private static SDL_AppIterate StoredIterate;
+    private static SDL_AppEvent StoredEvent;
+    private static SDL_AppInit StoredInit;
+    private static SDL_AppQuit StoredQuit;
+    
     // Main Function
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate int MainFunction(int argc, IntPtr argv);
+    
+    // App Init
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate SDL.AppResult SDL_AppInit(IntPtr state, int argc, IntPtr argv);
+
+    // App Iterate
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate SDL.AppResult SDL_AppIterate(IntPtr state);
+
+    // App Event
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate SDL.AppResult SDL_AppEvent(IntPtr state, SDL.Event* evt);
+
+    // App Quit
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void SDL_AppQuit(IntPtr state, SDL.AppResult result);
+    
+    // Enter App Main Callbacks
+    [DllImport(library, CallingConvention = CallingConvention.Cdecl)]
+    private static extern int SDL_EnterAppMainCallbacks(int argc, IntPtr argv, SDL_AppInit initFunc, SDL_AppIterate iterateFunc, SDL_AppEvent eventFunc, SDL_AppQuit quitFunc);
+    public static int EnterAppMainCallbacks(int argc, IntPtr argv, SDL_AppInit initFunc, SDL_AppIterate iterateFunc, SDL_AppEvent eventFunc, SDL_AppQuit quitFunc)
+    {
+        StoredIterate = iterateFunc;
+        StoredEvent = eventFunc;
+        StoredInit = initFunc;
+        StoredQuit = quitFunc;
+        
+        return SDL_EnterAppMainCallbacks(argc, argv, initFunc, iterateFunc, eventFunc, quitFunc);
+    }
     
     // Run App
     [DllImport(library, CallingConvention = CallingConvention.Cdecl)]
