4fc17f4de26e01f0456313755850738ae75ad067
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-01-11 13:44:54 +0000

    Debug
    * Removed debug by accident..

---
+++ Engine/Framework/Utility/Debug.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Hybrid
+{
+    // Debug API
+    public static class Debug
+    {
+        public static void Log(object message, bool trace = false)
+        {
+            if (trace)
+            {
+                Console.WriteLine($"[LOG] {message}\n{Environment.StackTrace}");
+                return;
+            }
+            
+            Console.WriteLine($"[LOG] {message}");
+        }
+        
+        public static void Warning(object message, bool trace = false)
+        {
+            if (trace)
+            {
+                Console.WriteLine($"[WARNING] {message}\n{Environment.StackTrace}");
+                return;
+            }
+            
+            Console.WriteLine($"[WARNING] {message}");
+        }
+        
+        public static void Error(object message, bool trace = false)
+        {
+            if (trace)
+            {
+                Console.WriteLine($"[ERROR] {message}\n{Environment.StackTrace}");
+                return;
+            }
+            
+            Console.WriteLine($"[ERROR] {message}");
+        }
+        
+        public static void Assert(bool condition, object message)
+        {
+            if (!condition)
+            {
+                throw new Exception($"[ASSERT] {message}");
+            }
+        }
+        
+        public static void Exception(object message)
+        {
+            throw new Exception($"[EXCEPTION] {message}");
+        }
+    }
+}
