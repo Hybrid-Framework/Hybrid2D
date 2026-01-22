f2bd55bce4330f92b2288201ee5eba4f47687cc4
Lloyd J Howarth <Lloyd.J.Howarth@gmail.com>
2026-01-22 14:12:29 +0000

    Framework
    * Restructure project

--- .github/scripts/Android.sh
+++ .github/scripts/Android.sh
@@ -4,7 +4,7 @@ set -e
 echo "Building... OS: $OS PLATFORM: $PLATFORM ARCH: $ARCH RID: $RID"
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-NATIVES_DIR="$BASE_DIR/../../Platforms/$PLATFORM/Natives"
+NATIVES_DIR="$BASE_DIR/../../Platforms/Mobile/$PLATFORM/Natives"
 MODULES_DIR="$BASE_DIR/Dependencies/Modules"
 DEPENDENCIES_DIR="$BASE_DIR/Dependencies"
 source "$DEPENDENCIES_DIR/Methods.sh"
